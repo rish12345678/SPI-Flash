3d6cb35c5c8461a89e1e91689f65217da531ddb0
Rishab Madhugiri <madhugiri.r@northeastern.edu>
2026-07-28 01:47:24 -0700

    Preliminary Flash Driver Complete; Test Next

--- Src/flash.c
+++ Src/flash.c
@@ -19,7 +19,7 @@ void Flash_Poll_Until_Ready(void) {
 
 		// Check it's zero'th bit, a zero is not BUSY and a 1 is BUSY
 		// if read status register says its NOT BUSY -> return
-		if (!(incoming_arr[1] && FLASH_SR1_STATUS_MSK)) return;
+		if (!(incoming_arr[1] & FLASH_SR1_STATUS_MSK)) return;
 	}
 }
 
@@ -62,9 +62,10 @@ void Flash_Page_Program(uint32_t adr, const uint8_t *buf, uint16_t len) {
 	 */
 
 	// Handle errors here: For now just return
-	if (adr <= 0x00FFFFFF) return;
-	if (len > 256) return;
-	if (buff == NULL) return;
+	if (adr < 0 || adr > 0x00FFFFFF) return;
+	// If pay-load larger than 256, too big for our transfer_arr
+	if (len > MAX_TRANSFER_LEN - 4) return;
+	if (buf == NULL) return;
 
 	// Send WE cmnd to flip on WEL flag
 	Flash_Set_Write_Enable();
@@ -89,3 +90,87 @@ void Flash_Page_Program(uint32_t adr, const uint8_t *buf, uint16_t len) {
 	// Call SPI Interrupt driver function to send transmission
 	SPI_Interrupt_Send_Payload(transfer_arr, incoming_arr, user_def_transfer_len);
 }
+
+void Flash_Read_Data(uint32_t adr, uint8_t *buf, uint16_t len) {
+	/*
+	 * Poll BSY, wait until not busy
+	 *
+	 * Now, load up 0x03 followed by adr of first byte to read, followed by num bytes read
+	 *
+	 * Send it
+	 */
+
+	if (adr < 0 || adr > 0x00FFFFFF) return;
+	// If read-load larger than 256, too big for our incoming_arr
+	if (len > MAX_TRANSFER_LEN - 4) return;
+	if (buff == NULL) return;
+
+
+
+	// Make sure SPI Chip is done with Write Enable cmnd
+	Flash_Poll_Until_Ready();
+
+	// Now that the chip is allowed to do a write + not occupied
+	transfer_arr[0] = FLASH_READ_DATA_CMND;
+
+	// Set the address bytes
+	// 0x00123456
+	transfer_arr[1] = (0xFF & (adr >> 16));
+	transfer_arr[2] = (0xFF & (adr >> 8));
+	transfer_arr[3] = (0xFF & adr);
+	// Set len whatever the user wants to send plus the four byte header
+	user_def_transfer_len = len + 4;
+
+	for (int i = 0; i < len; i++) {
+		transfer_arr[i + 4] = DUMMY;
+	}
+	// Call SPI Interrupt driver function to send transmission
+	SPI_Interrupt_Send_Payload(transfer_arr, incoming_arr, user_def_transfer_len);
+
+	// Wait for SPI transfer to finish
+	while (Get_Spi_State() != SPI_READY_STATE);
+
+	// Copy chip's memory from incoming_arr to user's buffer
+	for (int i = 0; i < len; i++) {
+		*(buf + i) = incoming_arr[i + 4];
+	}
+}
+
+void Flash_Erase_Sector(uint32_t adr) {
+	/*
+	 * Call Write Enable Instruction
+	 *
+	 * Poll until not busy
+	 *
+	 * Send over 0x20 followed by addr
+	 *
+	 * Once CS pulled high, starts the erase, for a sector takes 45 - 400 ms
+	 *
+	 * Then, WEL and BSY go to zero
+	 */
+
+
+	if (adr < 0 || adr > 0x00FFFFFF) return;
+	// Look into allowing unaligned erases later; rn keep aligned req
+	if (adr % 4096 != 0) return;
+
+	// Send WE cmnd to flip on WEL flag
+	Flash_Set_Write_Enable();
+
+	// Make sure SPI Chip is done with Write Enable cmnd
+	Flash_Poll_Until_Ready();
+
+	transfer_arr[0] = FLASH_SECTOR_ERASE_CMND;
+
+	// Set the address bytes
+	// 0x00123456
+	transfer_arr[1] = (0xFF & (adr >> 16));
+	transfer_arr[2] = (0xFF & (adr >> 8));
+	transfer_arr[3] = (0xFF & adr);
+
+	user_def_transfer_len = 4;
+
+	SPI_Interrupt_Send_Payload(transfer_arr, incoming_arr, user_def_transfer_len);
+}
+
+
