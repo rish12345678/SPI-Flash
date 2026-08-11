226805c60e635edd3a4ef3a8c07895c2e254bc74
Rishab Madhugiri <madhugiri.r@northeastern.edu>
2026-08-10 23:32:28 -0700

    Re-instated set_reset_meta() for Force Flash Reset Testing Compiler Flag

--- Src/ftl.c
+++ Src/ftl.c
@@ -238,14 +238,14 @@ static void mid_GC_powerloss_reboot(void) {
 	}
 }
 
-//static void set_reset_meta(void) {
-//	// Block erase both blocks
-//	uint32_t b0_adr = block_sector_page_offset_to_adr(0, 0, 0, 0);
-//	uint32_t b1_adr = block_sector_page_offset_to_adr(1, 0, 0, 0);
-//
-//	Flash_Erase_Block(b0_adr);
-//	Flash_Erase_Block(b1_adr);
-//}
+static void set_reset_meta(void) {
+	// Block erase both blocks
+	uint32_t b0_adr = block_sector_page_offset_to_adr(0, 0, 0, 0);
+	uint32_t b1_adr = block_sector_page_offset_to_adr(1, 0, 0, 0);
+
+	Flash_Erase_Block(b0_adr);
+	Flash_Erase_Block(b1_adr);
+}
 
 
 
